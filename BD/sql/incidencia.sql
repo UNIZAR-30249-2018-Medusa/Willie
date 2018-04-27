93fe4784f4d3a04474cecac0a2427fd929b4accd
Electryk <a.martinez1889@gmail.com>
2018-04-27 20:18:23 +0200

    Creacion de tabla incidencia con todos los cambos del objeto Incidencia y los campos de los objetos Embedded, cambiando el nombrado de columnas del formato camelCase al formato guion_bajo requerido por la dependencia de PostgreSQL.

---
+++ BD/sql/incidencia.sql
@@ -0,0 +1,30 @@
+-- Table: public.incidencia
+
+DROP TABLE public.incidencia;
+
+CREATE TABLE public.incidencia
+(
+    id uuid NOT NULL,
+    descripcion text COLLATE pg_catalog."default",
+    exterior boolean,
+    "es_notificacion" boolean,
+    "hora_fecha_creada" date,
+    idespacio text COLLATE pg_catalog."default",
+    latitud double precision,
+    longitud double precision,
+    planta integer,
+    "nombre_trabajador" text COLLATE pg_catalog."default",
+    username text COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default",
+    "codigo_cancelacion" double precision,
+    nombre text COLLATE pg_catalog."default",
+    "hora_fecha_completada" date,
+    CONSTRAINT incidencia_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.incidencia
+    OWNER to postgres;
