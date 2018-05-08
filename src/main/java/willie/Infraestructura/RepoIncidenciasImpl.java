af0a239a66b398906caffd922b7b816d289d42da
Iván Santamaría <ivansantamaria.m@gmail.com>
2018-05-08 14:04:12 +0200

    actualizado incidencia repositorio y añadido titulo a incidencia

--- src/main/java/willie/Infraestructura/RepoIncidenciasImpl.java
+++ src/main/java/willie/Infraestructura/RepoIncidenciasImpl.java
@@ -4,9 +4,50 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import willie.dominio.Incidencia;
 import willie.dominio.RepositorioIncidencias;
-
 import java.util.UUID;
 
 @Repository
-public interface RepoIncidenciasImpl extends RepositorioIncidencias {
+public interface RepoIncidenciasImpl implements RepositorioIncidencias {
+
+    @Override
+    void anyadirIncidencia(String descripcion, String nombre, Localizacion localizacion, Trabajador trabajador)
+    {
+
+    }
+
+    @Override
+    void actualizarIncidencia(String descripcion, String nombre, Localizacion localizacion, Trabajador trabajador)
+    {
+
+    }
+
+    @Override
+    void borrarIncidencia(String nombre)
+    {
+
+    }
+
+    @Override
+    void cambiarEstadoIncidencia(String nombre, Estado estado)
+    {
+
+    }
+
+    @Override
+    Incidencia buscarIncidenciaNombre(String nombre)
+    {
+
+    }
+
+    @Override
+    ArrayList<Incidencia> buscarIncidenciasTrabajador(Trabajador trabajador)
+    {
+
+    }
+
+    @Override
+    Localizacion localizarIncidencia(String nombre)
+    {
+
+    }
 }
