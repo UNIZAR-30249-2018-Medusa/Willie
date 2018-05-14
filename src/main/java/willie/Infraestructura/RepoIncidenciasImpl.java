b771579790059b109fa1005ac747806370ef54fd
fiusa8 <dfusteropalacios@gmail.com>
2018-05-14 14:56:43 +0200

    Consultas insertar, actualizar y borrar

--- src/main/java/willie/Infraestructura/RepoIncidenciasImpl.java
+++ src/main/java/willie/Infraestructura/RepoIncidenciasImpl.java
@@ -1,6 +1,8 @@
 package willie.Infraestructura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import willie.dominio.*;
 
@@ -10,23 +12,52 @@ import java.util.UUID;
 @Repository
 public class RepoIncidenciasImpl implements RepositorioIncidencias {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
-    public void anyadirIncidencia(String descripcion, String nombre, Localizacion localizacion, Trabajador trabajador) {
+    public void anyadirIncidencia(String descripcion, String nombre,
+                                  Localizacion localizacion, Trabajador trabajador) {
+
+        final String SQLstatement = "INSERT INTO public.incidencia (" +
+                "descripcion, nombre, localizacion, trabajador)" +
+                " VALUES (?, ?, ?, ?)";
+
+        jdbcTemplate.update(SQLstatement, descripcion, nombre,
+                localizacion, trabajador);
 
     }
 
     @Override
-    public void actualizarIncidencia(String descripcion, String nombre, Localizacion localizacion, Trabajador trabajador) {
+    public void actualizarIncidencia(String descripcion, String nombre,
+                                     Localizacion localizacion, Trabajador trabajador) {
+
+        final String SQLstatement = "UPDATE public.incidencia SET " +
+                "descripcion = ?, localizacion = ?, trabajador = ? " +
+                "WHERE nombre = ?";
+
+        jdbcTemplate.update(SQLstatement, descripcion,
+                localizacion, trabajador, nombre);
 
     }
 
     @Override
     public void borrarIncidencia(String nombre) {
 
+        final String SQLstatement = "DELETE FROM public.incidencia " +
+                "WHERE nombre = ?";
+
+        jdbcTemplate.update(SQLstatement, nombre);
+
     }
 
     @Override
     public void cambiarEstadoIncidencia(String nombre, Estado estado) {
+
+        final String SQLstatement = "UPDATE public.incidencia SET " +
+                "estado = ? WHERE nombre = ?";
+
+        jdbcTemplate.update(SQLstatement, estado, nombre);
 
     }
 
